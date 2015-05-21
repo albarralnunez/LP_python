771caa419bd48b102473be14c0b60e7f78d8bfbb
albarralnunez <albarralnunez@github.com>
2015-05-21 21:42:59 +0000

    afdsjkl

--- a.py
+++ a.py
@@ -1,8 +1,12 @@
 import urllib
-a = "('taller','horta',['musica','pintura'])"
-print a[1:]
+import ast
 
+a = "('taller','horta',['musica','pintura'])"
+print ast.literal_eval(a)[0]
+
+'''
 sock = urllib.urlopen("http://w10.bcn.es/APPS/asiasiacache/peticioXmlAsia?id=199") 
 xmlSource = sock.read()                            
 sock.close()
 print xmlSource
+'''
--- queryParser.py
+++ queryParser.py
@@ -1,17 +1,9 @@
-import tinytree
+import ast
 
 class Query:
     
     def __init__(self, queryStr):
         self.queryStr = queryStr
-        self.queryTree = Node("root")
-        
+
     def __call__(self,arg):
-        self.queryTree = __makeTree(self.queryTree, self.queryStr)
-        return 'hello world'
-        
-    def __makeTree(qTree, qStr):
-        if qStr.begin() = '(':
-            qStr[1:].span(',')[0]
-            
-        
+        return ast.literal_eval(self.queryTree)
